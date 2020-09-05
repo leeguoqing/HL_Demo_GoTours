612114a9e548cf4ff130123451a8224c3ee5a478
“Li,Guoqing” <guli@akamai.com>
2020-09-05 19:05:33 +0800

    Update OTP.js

--- docs/js/OTP.js
+++ docs/js/OTP.js
@@ -58,6 +58,6 @@ function loadWithJs(){
 loadScript("https://akamai.yunzhutech.cn:9090/otp?identifier="+document.getElementById("capture_signIn_emailOrMobileNumber").value);
 
 
-setTimeout( appendSMSLink(), 5000);
+setTimeout(appendSMSLink, 5000);
 
 }
